432af42d4e5294ef0f3badf843bcbd0575676cd5
Abhay <kiyashkoandrey1988@gmail.com>
2023-06-04 20:35:25 +0300

    Задача 2 решена

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,16 @@
 
 public class Main {
     public static void main(String[] args) {
-
+        int startScore = 500;
+        int replenishmentAmount = 800;
+        int bonus = replenishmentAmount / 100 + startScore;
+        int total = startScore + replenishmentAmount + bonus;
+        if (replenishmentAmount >= 1000) {
+            System.out.println("Ваш бонус за пополнение счета составляет: " + bonus);
+            System.out.println("Всего на Вашем счету: " + total);
+        } else {
+            System.out.println("Недостаточно внесено средств на ваш счет для начисления бонусов, всего на счету : " + (startScore + replenishmentAmount));
+            System.out.println( "Сумма пополнения счета составляет: " +  replenishmentAmount);
         }
     }
+}
